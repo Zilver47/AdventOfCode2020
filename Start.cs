2975b82910e95f213ba3b5f6ea360858f8f35bac
Zilver47 <a.geertsema@belastingdienst.nl>
2020-12-01 14:15:21 +0100

    Day 1 two

--- Start.cs
+++ Start.cs
@@ -4,7 +4,7 @@ public static class Start
 {
     static void Main()
     {
-        var generator = new Day1One();
+        var generator = new Day1Two();
 
         Console.WriteLine("Answer: " + generator.Generate());
         Console.ReadLine();
