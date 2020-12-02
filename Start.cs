ac3fbec1968f6c5bcf512a281fd7dfcc464e15d0
Zilver47 <arian@zilverstede.nl>
2020-12-02 08:02:51 +0100

    Day 2

--- Start.cs
+++ Start.cs
@@ -4,7 +4,7 @@ public static class Start
 {
     static void Main()
     {
-        var generator = new Day1Two();
+        var generator = new Day2Two();
 
         Console.WriteLine("Answer: " + generator.Generate());
         Console.ReadLine();
