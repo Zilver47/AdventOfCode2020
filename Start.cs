24e6841c79b6fcac96a9ec194292c60085072f9b
Zilver47 <a.geertsema@belastingdienst.nl>
2020-12-02 10:33:07 +0100

    Refactoring

--- Start.cs
+++ Start.cs
@@ -1,10 +1,12 @@
 using System;
+using System.IO;
 
 public static class Start
 {
     static void Main()
     {
-        var generator = new Day2Two();
+        var lines = File.ReadAllLines("input.txt");
+        var generator = new Day2.One(lines);
 
         Console.WriteLine("Answer: " + generator.Generate());
         Console.ReadLine();
